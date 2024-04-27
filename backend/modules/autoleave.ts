47b25e761708ced1003b95e4842c4535267a62c8
Elliott <85990359+Ell1ott@users.noreply.github.com>
2024-04-27 17:37:29 +0200

    Update autoleave.ts

--- backend/modules/autoleave.ts
+++ backend/modules/autoleave.ts
@@ -1,34 +1,36 @@
-import { BotInstance } from "../bot";
 import { Module } from "./module";
 
 class AutoLeave extends Module {
-	playerJoined(player) {
+	onPlayerJoined(player) {
 		if (!this.settings.playerjoin.enabled) return;
 		if (this.settings.playerjoin.val.includes(player.username)) {
-			bot.quit();
+			this.bot.quit();
 			this.info("left beacuse " + player.username + " joined");
 		}
 	}
 
-	healthChange() {
-		if (this.settings.health.enabled && bot.health < this.settings.health.val) {
-			bot.quit();
+	onHealthChange() {
+		if (
+			this.settings.health.enabled &&
+			this.bot.health < this.settings.health.val
+		) {
+			this.bot.quit();
 			this.info("left beacuse health got below " + this.settings.health.val);
 			return;
 		}
-		if (this.settings.food.enabled && bot.food < this.settings.food.val) {
-			bot.quit();
+		if (this.settings.food.enabled && this.bot.food < this.settings.food.val) {
+			this.bot.quit();
 			this.info("left beacuse food got below " + this.settings.food.val);
 
 			return;
 		}
 	}
 
-	entityMoved(e) {
+	onEntityMoved(e) {
 		if (!this.settings.player_too_close.enabled) return;
 		// console.log();
 		if (e.type !== "player") return;
-		const dis = bot.entity.position.distanceTo(e.position);
+		const dis = this.bot.entity.position.distanceTo(e.position);
 		if (
 			this.settings.player_too_close.players.val.some(
 				(item) => item.toLowerCase() === e.username.toLowerCase()
@@ -37,8 +39,8 @@ class AutoLeave extends Module {
 			if (!this.settings.player_too_close.specifiedplayerdistance.enabled)
 				return;
 			if (dis < this.settings.player_too_close.specifiedplayerdistance.val[0]) {
-				bot.quit();
-				bot.removeListener("entityMoved", this.entityMoved);
+				this.bot.quit();
+				this.bot.removeListener("entityMoved", this.onEntityMoved);
 
 				this.info("left beacuse " + e.username + " got too close");
 			}
@@ -47,23 +49,23 @@ class AutoLeave extends Module {
 			dis < this.settings.player_too_close.otherplayerdistance.val[0]
 		) {
 			if (!this.settings.player_too_close.otherplayerdistance.enabled) return;
-			bot.quit();
-			bot.removeListener("entityMoved", this.entityMoved);
+			this.bot.quit();
+			this.bot.removeListener("entityMoved", this.onEntityMoved);
 
 			this.info("left beacuse " + e.username + " got too close");
 			return;
 		}
 	}
 	start() {
-		bot.on("playerJoined", this.playerJoined);
-		bot.on("health", this.healthChange);
-		bot.on("entityMoved", this.entityMoved);
+		this.bot.on("playerJoined", this.onPlayerJoined);
+		this.bot.on("health", this.onHealthChange);
+		this.bot.on("entityMoved", this.onEntityMoved);
 	}
 
 	stop() {
-		bot.removeListener("playerJoined", this.playerJoined);
-		bot.removeListener("health", this.healthChange);
-		bot.removeListener("entityMoved", this.entityMoved);
+		this.bot.removeListener("playerJoined", this.onPlayerJoined);
+		this.bot.removeListener("health", this.onHealthChange);
+		this.bot.removeListener("entityMoved", this.onEntityMoved);
 	}
 }
 
