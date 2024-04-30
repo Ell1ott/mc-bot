3b628d6e6841710b4ff3600587e44e43884884ca
Ell1ott <85990359+Ell1ott@users.noreply.github.com>
2024-04-30 13:28:47 +0200

    Backend: Begin multi bot implementation

--- backend/index.ts
+++ backend/index.ts
@@ -19,6 +19,10 @@ testBot.joinLocalhost(45486);
 
 testBot.start();
 
+let currentBot: BotInstance = testBot;
+
+const bots = { testBot: testBot };
+
 io.on("connection", (socket) => {
 	console.log("a user connected");
 
@@ -30,11 +34,26 @@ io.on("connection", (socket) => {
 		console.log("a user disconnected");
 	});
 
-	socket.onAny((event, ...args) => {
-		testBot.client.emit(event, ...args);
+	socket.on("newBot", (name) => {
+		const newBot = new BotInstance(exportSettings(name));
+		bots[name] = newBot;
 	});
 
-	testBot.io.onAny((event, ...args) => {
-		socket.emit(event, ...args);
+	let anyListener = (event, ...args) => {
+		testBot.client.emit(event, ...args);
+	};
+	socket.on("selectBot", (name) => {
+		currentBot.io.offAny();
+		socket.offAny(anyListener);
+
+		currentBot = bots[name];
+
+		anyListener = (event, ...args) => {
+			currentBot.client.emit(event, ...args);
+		};
+		socket.onAny(anyListener);
+		currentBot.io.onAny((event, ...args) => {
+			socket.emit(event, ...args);
+		});
 	});
 });
