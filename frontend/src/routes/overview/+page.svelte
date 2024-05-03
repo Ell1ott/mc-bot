47e5e3c26f4fdb1199f2c53cf79e743a35037d1c
Elliott <85990359+Ell1ott@users.noreply.github.com>
2024-05-03 13:27:13 +0200

    Add shadcn comps

--- frontend/src/routes/overview/+page.svelte
+++ frontend/src/routes/overview/+page.svelte
@@ -9,8 +9,30 @@
 
 	import food from '$lib/food/food.png';
 	import bgfood from '$lib/food/Untitled.png';
+	import Button from '$lib/components/ui/button/button.svelte';
+	import Card from '$lib/components/ui/card/card.svelte';
 
-	let bots: any = null;
+	// let bots: any = null;
+	let bots = [
+		{
+			id: 'bot1',
+			name: 'E2SY',
+			health: 20,
+			food: 20
+		},
+		{
+			id: 'bot2',
+			name: 'Bot',
+			health: 20,
+			food: 20
+		},
+		{
+			id: 'bot3',
+			name: 'Notch',
+			health: 20,
+			food: 20
+		}
+	];
 
 	currentBot.set(null);
 	$: $socket?.emit('deselectBot');
@@ -31,11 +53,11 @@
 </script>
 
 <div class="p-4 space-y-4">
-	<div class="flex gap-2">
+	<div class="flex gap-4 flex-wrap">
 		{#if bots}
 			{#each bots as bot}
-				<button
-					class="flex gap-2 p-3 rounded-md bg-gray-700 hover:bg-gray-600 transition-all"
+				<Card
+					class="flex gap-2 p-4 rounded-md hover:bg-accent transition-all hover:border-foreground/20 cursor-pointer"
 					on:click={() => selectBot(bot.id)}
 				>
 					<div class="w-20 aspect-square">
@@ -43,7 +65,7 @@
 					</div>
 					<div class="flex">
 						<div class="flex flex-col max-h-[6rem] items-start min-w-[10rem] gap-1">
-							<h1 class="font-bold text-3xl">{bot.name}</h1>
+							<h1 class="font-bold text-3xl text-foreground">{bot.name}</h1>
 							{#if bot.health}
 								<Statbar img={heart} bgimg={bgheart} stat={bot.health} />
 							{/if}
@@ -57,9 +79,12 @@
 							{/if}
 						</div>
 					</div>
-				</button>
+				</Card>
 			{/each}
 		{/if}
 	</div>
-	<Button>Button</Button>
+	<Button class="gap-2" variant="secondary">
+		<Plus size="20" />
+		New Bot
+	</Button>
 </div>
