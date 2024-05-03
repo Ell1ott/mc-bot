5b83de6d1226002946ab908b9c5da7da2357ec78
Elliott <85990359+Ell1ott@users.noreply.github.com>
2024-05-03 14:24:07 +0200

    Improve

--- frontend/src/routes/overview/newbot.svelte
+++ frontend/src/routes/overview/newbot.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+	import { server_loads } from './../../../.svelte-kit/generated/client/app.js';
+	import AlertDialogCancel from './../../lib/components/ui/alert-dialog/alert-dialog-cancel.svelte';
+	import AlertDialogFooter from './../../lib/components/ui/alert-dialog/alert-dialog-footer.svelte';
 	import { AlertDialog, AlertDialogTrigger } from '$lib/components/ui/alert-dialog';
 	import AlertDialogContent from '$lib/components/ui/alert-dialog/alert-dialog-content.svelte';
 	import AlertDialogHeader from '$lib/components/ui/alert-dialog/alert-dialog-header.svelte';
@@ -21,17 +24,47 @@
 		</Button>
 	</AlertDialogTrigger>
 
-	<AlertDialogContent>
+	<AlertDialogContent class="bg-card">
 		<AlertDialogHeader>New bot</AlertDialogHeader>
-		<Tabs>
-			<TabsList class="grid w-full grid-cols-2">
-				<TabsTrigger value="offline">Offline</TabsTrigger>
-				<TabsTrigger value="microsoft">Microsoft</TabsTrigger>
-			</TabsList>
-			<TabsContent value="offline">
-				<Label>Bot name</Label>
-				<Input placeholder="Bot"></Input>
-			</TabsContent>
-		</Tabs>
+		<div class="flex gap-4 flex-col justify-stretch items-stretch">
+			<Tabs class="flex gap-2">
+				<div>
+					<Label>Login method</Label>
+					<TabsList class="grid w-full grid-cols-2">
+						<TabsTrigger value="offline">Offline</TabsTrigger>
+						<TabsTrigger value="microsoft">Microsoft</TabsTrigger>
+					</TabsList>
+				</div>
+				<TabsContent value="offline" class="mt-0 flex-1">
+					<Label>Bot name</Label>
+					<Input placeholder="Bot"></Input>
+				</TabsContent>
+				<TabsContent value="microsoft" class="mt-0 flex-1">
+					<Label>Email</Label>
+					<Input type="mail" placeholder="example@mail.com"></Input>
+				</TabsContent>
+			</Tabs>
+			<Tabs class="flex gap-2">
+				<div>
+					<Label>Server type</Label>
+					<TabsList class="grid w-full grid-cols-2">
+						<TabsTrigger value="server">Server</TabsTrigger>
+						<TabsTrigger value="localhost">Localhost</TabsTrigger>
+					</TabsList>
+				</div>
+				<TabsContent value="localhost" class="mt-0 flex-1">
+					<Label>Port</Label>
+					<Input placeholder="25565"></Input>
+				</TabsContent>
+				<TabsContent value="server" class="mt-0 flex-1">
+					<Label>Server IP</Label>
+					<Input placeholder="hypixel.net"></Input>
+				</TabsContent>
+			</Tabs>
+		</div>
+		<AlertDialogFooter>
+			<AlertDialogCancel>Cancel</AlertDialogCancel>
+			<Button>Create</Button>
+		</AlertDialogFooter>
 	</AlertDialogContent>
 </AlertDialog>
