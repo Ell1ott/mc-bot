9cd080fef8d5ba702fbbfd7aa73f0fe93d94bb56
Elliott <85990359+Ell1ott@users.noreply.github.com>
2024-05-09 23:08:04 +0200

    Frontend: Try to fix blurry text

--- frontend/src/lib/components/NoSettings.svelte
+++ frontend/src/lib/components/NoSettings.svelte
@@ -1,10 +1,1 @@
-<p class="m-0 p-0">no settings available</p>
-
-<style>
-	p {
-		padding: 0;
-		margin: 0;
-
-		color: rgb(123, 123, 123);
-	}
-</style>
+<p class="m-0 p-0 text-muted-foreground">No settings available</p>
